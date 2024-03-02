54565ab567524e481be0014da1e29f95f989c587
weicheng2138 <weicheng2138@gmail.com>
2024-03-02 18:06:11 +0800

    chore(theme): add theme provider

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,6 @@ module.exports = {
     ],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'react-refresh/only-export-components': 'off',
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
-import { Button } from './components/ui/button';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/mode-toggle';
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1>Vite + React</h1>
       <Button>YOYOYO</Button>
-    </>
+      <ModeToggle />
+    </ThemeProvider>
   );
 }
 
