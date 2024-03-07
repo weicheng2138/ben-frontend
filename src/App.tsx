5bca921f7471f1e9ff192a36ce123ed9f9ffc281
weicheng2138 <weicheng2138@gmail.com>
2024-03-07 14:17:31 +0800

    chore(app): clean up redundant css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode-toggle';
 
 function App() {
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log('drag over');
+  };
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <h1>Vite + React</h1>
-      <Button>YOYOYO</Button>
-      <ModeToggle />
+      <div
+        className="flex h-dvh items-center justify-center"
+        onDragOver={handleDragOver}
+      >
+        <h1>Vite + React</h1>
+        <Button>YOYOYO</Button>
+        <ModeToggle />
+      </div>
     </ThemeProvider>
   );
 }
