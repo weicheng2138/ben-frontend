305f6a2e450e024fdd1ee23dc629b4f72238d14f
weicheng2138 <weicheng2138@gmail.com>
2024-03-19 13:31:13 +0800

    style: change a bit of layout to fix step1 problem

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex w-full flex-col items-center">
+      <div className="h-svhh flex w-full flex-col items-center">
         <header
           className={cn(
             'sticky top-0 flex h-16 w-full items-center justify-center backdrop-blur',
@@ -40,25 +40,25 @@ function App() {
         </header>
 
         <main className="mt-1 flex w-full max-w-3xl flex-col gap-4 px-6">
-          <h1 className="mb-4 text-center text-5xl font-extrabold">Vite</h1>
-          <div className="flex w-full flex-col gap-4">
-            <section className="flex w-full items-center justify-start gap-2">
-              <h2 className="text-nowrap text-sm font-black">步驟一</h2>
-              <p className="text-sm font-light">
-                請將影片檔案拖曳至虛線方框內，或是點選「上傳影片檔案」。
-              </p>
-            </section>
-            <CustomUpload
-              isUploading={isUploading}
-              onUpload={handleUpload}
-              className="h-[30dvh]"
-            />
-          </div>
+          <h1 className="mb-2 text-center text-xl font-extrabold md:mb-4 md:text-5xl">
+            Vite
+          </h1>
+          <section className="flex w-full items-center justify-start gap-2">
+            <h2 className="text-nowrap text-sm font-black">步驟一</h2>
+            <p className="text-sm font-light">
+              請將影片檔案拖曳至虛線方框內，或是點選「上傳影片檔案」。
+            </p>
+          </section>
+          <CustomUpload
+            isUploading={isUploading}
+            onUpload={handleUpload}
+            className="h-[30svh]"
+          />
           <section className="mt-4 flex w-full items-center justify-start gap-2">
             <h2 className="text-nowrap text-sm font-black">步驟二</h2>
             <p className="text-sm font-light">下載影片。</p>
           </section>
-          <ScrollResult className="h-[30dvh]" ref={resultRef} />
+          <ScrollResult className="h-[30svh]" ref={resultRef} />
         </main>
       </div>
 
